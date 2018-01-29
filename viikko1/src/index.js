e285ced1e3b3716e1e95d1d4090087bb92925c68
Aurora Tulilaulu <aurora.tulilaulu@codemate.com>
2018-01-29 13:59:05 +0200

    teht 1.6

--- viikko1/src/index.js
+++ viikko1/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Otsikko = (props) => {
+/*const Otsikko = (props) => {
   
   return (
     <h1>{props.kurssi.nimi}</h1>
@@ -32,7 +32,7 @@ const Sisalto = (props) => {
     )
   }
 
-const App = () => {
+const App = () => { //Ensimmäinen tehtäväsetti
     const kurssi = {
         nimi: 'Half Stack -sovelluskehitys',
         osat: [
@@ -58,8 +58,40 @@ const App = () => {
       </div>
     )
   }
-
-ReactDOM.render(
+  ReactDOM.render(
   <App />,
   document.getElementById('root')
-)
+)*/
+
+class App extends React.Component {
+  //tehtäväsetti 2
+    constructor(props) {
+        super(props)
+        this.state = {
+          good: 0,
+          neutral: 0,
+          bad: 0
+        }
+      }
+    
+      render() {
+        return (
+          <div>
+            <h2>Anna palautetta</h2>
+            <button onClick={()=> this.setState({good: this.state.good + 1})}>Hyvä</button>
+            <button onClick={()=> this.setState({neutral: this.state.neutral + 1})}>Neutraali</button>
+            <button onClick={()=> this.setState({bad: this.state.bad + 1})}>Huono</button>
+            <h2>Statistiikka</h2>
+            <div>Hyvä: {this.state.good}</div>
+            <div>Neutraali: {this.state.neutral}</div>
+            <div>Huono: {this.state.bad}</div>
+          </div>
+        )
+      }
+    }
+    
+    ReactDOM.render(
+      <App />,
+      document.getElementById('root')
+    )
+
